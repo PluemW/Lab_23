59b3bc8a2881cf2d331bcb634bfe44523602ce2d
PluemW <96386038+PluemW@users.noreply.github.com>
2023-03-03 15:02:41 +0700

    Filled

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc, char *argv[]){
+    double sum;
+    if(argc>1){
+        for(int i=0; i<argc-1; i++){
+            sum += atol(argv[i+1]);
+        }
+        double aver = sum/(argc-1);
+        cout<<"---------------------------------\nAverage of "<< argc-1 <<" numbers = "<< aver;
+        cout<<"\n---------------------------------";
+    }
+    else{
+        cout << "Please input numbers to find average.";
+    }
+}
